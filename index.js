84f00cfc4347497183fa78b1664778b19dba9a4d
MuazzazA <ayd.muazzez@gmail.com>
2022-10-27 14:14:25 +0300

    test

---
+++ index.js
@@ -0,0 +1,11 @@
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
+
+// AppRegistry.registerComponent(appName, () => App);
+
+// import registerRootComponent from 'expo/build/launch/registerRootComponent';
+
+// import App from '../../App';
+
+// registerRootComponent(App);
