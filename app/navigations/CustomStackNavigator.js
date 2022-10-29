0c7bf5ae51c57d20767c0aeb6699193130fa8c50
MuazzazA <ayd.muazzez@gmail.com>
2022-10-29 21:00:23 +0300

    navigator oluşturuldu ve App.js bağlantısı + font

---
+++ app/navigations/CustomStackNavigator.js
@@ -0,0 +1,44 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { Login, Register, Onboarding } from "../screens/Auth/";
+import Home from "../screens/Home";
+import Home2 from "../screens/home2";
+
+const Stack = createNativeStackNavigator();
+
+const Navigations = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Onboarding">
+        <Stack.Screen
+          name="Onboarding"
+          component={Onboarding}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={Register}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home2"
+          component={Home2}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+export default Navigations;
