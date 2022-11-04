0b1242806ac5eba7dbf0ce14e01dec30e1f33aa2
MuazzazA <ayd.muazzez@gmail.com>
2022-11-04 12:13:43 +0300

    icon indexe eklendi

---
+++ app/constants/icons.js
@@ -0,0 +1,22 @@
+export const myCard = require("../assets/icons/pngCard.png");
+export const people = require("../assets/icons/people.png");
+export const sharedCard = require("../assets/icons/giveCard.png");
+export const cards = require("../assets/icons/cards.png");
+export const inbox = require("../assets/icons/inbox.png");
+export const sharedArrow = require("../assets/icons/sharedArrow.png");
+export const statistic = require("../assets/icons/statistics.png");
+export const share = require("../assets/icons/share.png");
+export const menu = require("../assets/icons/menuIcon.png");
+export const alertIcon = require("../assets/icons/alertIcon.png");
+export default {
+  myCard,
+  people,
+  sharedCard,
+  cards,
+  inbox,
+  sharedArrow,
+  statistic,
+  share,
+  menu,
+  alertIcon,
+};
