766241f6c5b54da68c089afcfadbab74be645af6
MuazzazA <ayd.muazzez@gmail.com>
2022-11-04 12:17:02 +0300

    anasayfa ekranları yazıldı - alert -drawer - iconbuttons

---
+++ app/screens/StatisticScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const StatisticCardScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>kartlar ile ilgili istatistikler</Text>
+    </View>
+  );
+};
+export default StatisticCardScreen;
