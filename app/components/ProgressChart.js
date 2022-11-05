d5f592939904cfdca22d5923ea05bf6379b02201
MuazzazA <ayd.muazzez@gmail.com>
2022-11-05 17:46:22 +0300

    progres chart düzenleme

--- app/components/ProgressChart.js
+++ app/components/ProgressChart.js
@@ -12,23 +12,21 @@ const CustomProgressChart = ({ data = [], descriptionText = "statistic" }) => {
       <ProgressChart
         data={data}
         width={SIZES.width / 2 - 10}
-        height={200}
+        height={140}
         chartConfig={{
           backgroundColor: "#1cc910",
           backgroundGradientFrom: COLORS.lightGrey,
           backgroundGradientTo: COLORS.lightGrey,
           color: (opacity = 1) => `rgba(120, 90, 90, ${opacity})`,
-          style: {
-            borderRadius: 16,
-            margin: 2,
-          },
         }}
-        style={{ borderRadius: 16 }}
+        style={{ borderRadius: SIZES.radius }}
       />
       <Text
         style={{
+          color: "#785a5a",
           width: SIZES.width / 2 - 10,
           ...FONTS.desc2,
+          paddingLeft: 13,
         }}>
         {descriptionText}
       </Text>
