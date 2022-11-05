97ffbf2fbdfff3f280f800cf25e52e14fe0917fa
MuazzazA <ayd.muazzez@gmail.com>
2022-11-05 17:56:39 +0300

    custom drawer yazım düzeltme

--- app/navigations/CustomDrawerNavigator.js
+++ app/navigations/CustomDrawerNavigator.js
@@ -1,39 +1,104 @@
 import * as React from "react";
 
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+  useDrawerProgress,
+  ollView,
+} from "@react-navigation/drawer";
 
-import Home from "../screens/Home";
+import { Text, View, Image } from "react-native";
+import { Home, AlertScreen } from "../screens/";
 import InsideNavigations from "./InsideStackNavigator";
-import AlertScreen from "../screens/AlertScreen";
+import { SIZES, FONTS, COLORS } from "../constants/theme";
+import { icons } from "../constants";
+const { avatar } = icons;
 
 const Drawer = createDrawerNavigator();
+
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <View
+        style={{
+          //backgroundColor: "beige",
+          flex: 1,
+        }}>
+        <DrawerItemList {...props} />
+
+        <DrawerItem
+          label="Çıkış Yap"
+          activeTintColor="white"
+          style={{
+            ...FONTS.desc1,
+          }}
+          onPress={() => alert("Çıkış İşlemi")}
+        />
+      </View>
+    </DrawerContentScrollView>
+  );
+}
+
+function CustomAvatarContent() {
+  return (
+    <View>
+      <Image
+        source={avatar}
+        style={{
+          height: 100,
+          width: 100,
+          alignSelf: "center",
+          backgroundColor: COLORS.lightGrey,
+          borderRadius: 50,
+        }}
+      />
+      <Text
+        style={{
+          textAlign: "center",
+          marginTop: SIZES.padding,
+          ...FONTS.body3,
+        }}>
+        isim soyisim
+      </Text>
+    </View>
+  );
+}
 
 const CustomDrawerNavigator = () => {
   return (
     <Drawer.Navigator
+      drawerContent={(props) => (
+        <View style={{ flex: 1, paddingTop: SIZES.h1 }}>
+          <CustomAvatarContent />
+          <CustomDrawerContent {...props} />
+        </View>
+      )}
       useLegacyImplementation
       initialRouteName="InsideNavigations"
-      screenOptions={{ headerShown: false }}>
-      <Drawer.Screen
-        name="InsideNavigations"
-        component={InsideNavigations}
-        options={{ headerShown: false, name: "Home" }}
-      />
-      <Drawer.Screen
-        name="Settings"
-        component={Home}
-        options={{ headerShown: false }}
-      />
-      <Drawer.Screen
-        name="Profil"
-        component={Home}
-        options={{ headerShown: false }}
-      />
-      <Drawer.Screen
-        name="AlertScreen"
-        component={AlertScreen}
-        options={{ headerShown: false }}
-      />
+      screenOptions={{
+        headerShown: false,
+        drawerStyle: {
+          backgroundColor: COLORS.softBlue,
+        },
+        drawerLabelStyle: {
+          ...FONTS.h4,
+          color: COLORS.black,
+          activeTintColor: COLORS.blue2,
+        },
+        drawerContentOptions: {
+          activeTintColor: COLORS.white,
+          ...FONTS.desc1,
+        },
+      }}
+      options={{
+        swipeEdgeWidth: 40,
+      }}>
+      <Drawer.Screen name="InsideNavigations" component={InsideNavigations} />
+      <Drawer.Screen name="Settings" component={Home} />
+      <Drawer.Screen name="Profil" component={Home} />
+      <Drawer.Screen name="AlertScreen" component={AlertScreen} />
     </Drawer.Navigator>
   );
 };
