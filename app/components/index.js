4017b9a6d5ac459da39b2c1c04e54fbdcb722441
MuazzazA <ayd.muazzez@gmail.com>
2022-11-10 16:08:39 +0300

    index.js component importları eklendi

--- app/components/index.js
+++ app/components/index.js
@@ -2,4 +2,18 @@ import CustomTextInput from "./TextInput";
 import CustomButton from "./Button";
 import CustomHeader from "./Header";
 import TextButton from "./TextButton";
-export { CustomTextInput, CustomButton, CustomHeader, TextButton };
+import Seperator from "./Seperator";
+import IconButton from "./IconButton";
+import SearchBar from "./SearchBar";
+import CustomProgressChart from "./ProgressChart";
+
+export {
+  CustomTextInput,
+  CustomButton,
+  CustomHeader,
+  TextButton,
+  Seperator,
+  IconButton,
+  SearchBar,
+  CustomProgressChart,
+};
