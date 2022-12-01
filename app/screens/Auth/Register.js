1bae5898ad20e0ab34c342447e4c0392baf0be1e
MuazzazA <ayd.muazzez@gmail.com>
2022-12-01 14:46:03 +0300

    register fix

--- app/screens/Auth/Register.js
+++ app/screens/Auth/Register.js
@@ -1,21 +1,25 @@
-import React from "react";
-import {
-  View,
-  Text,
-  StatusBar,
-  StyleSheet,
-  ScrollView,
-  Image,
-  TouchableOpacity,
-} from "react-native";
+import React, { useState } from "react";
+import { View, Text, ScrollView, Image } from "react-native";
 
 import { theme, images } from "../../constants";
-import { CustomButton, CustomTextInput, TextButton } from "../../components/";
+import {
+  CustomButton,
+  CustomTextInput,
+  TextButton,
+  ToastMessage,
+} from "../../components/";
 
 const { creditcard } = images;
-const { COLORS, SIZES, FONTS } = theme;
+const { SIZES, FONTS } = theme;
 
 const Register = ({ navigation }) => {
+  const [nick, setNick] = useState("");
+  const [userName, setUserName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+
   return (
     <ScrollView
       style={{
@@ -37,12 +41,36 @@ const Register = ({ navigation }) => {
         />
         <Text style={{ ...FONTS.body2, marginVertical: 20 }}>YENİ KAYIT</Text>
         <View>
-          <CustomTextInput placeHolderText="Kullanıcı Adı" />
-          <CustomTextInput placeHolderText="İsim" />
-          <CustomTextInput placeHolderText="Soyisim" />
-          <CustomTextInput placeHolderText="Telefon Numarası" />
-          <CustomTextInput placeHolderText="Şifre" />
-          <CustomTextInput placeHolderText="Şifre Tekrar" />
+          <CustomTextInput
+            placeHolderText="Kullanıcı Adı"
+            value={nick}
+            onChangeFunc={(e) => setNick(e.nativeEvent.text)}
+          />
+          <CustomTextInput
+            placeHolderText="İsim"
+            value={userName}
+            onChangeFunc={(e) => setUserName(e.nativeEvent.text)}
+          />
+          <CustomTextInput
+            placeHolderText="Soyisim"
+            value={surname}
+            onChangeFunc={(e) => setSurname(e.nativeEvent.text)}
+          />
+          <CustomTextInput
+            placeHolderText="Telefon Numarası"
+            value={phone}
+            onChangeFunc={(e) => setPhone(e.nativeEvent.text)}
+          />
+          <CustomTextInput
+            placeHolderText="Şifre"
+            value={password}
+            onChangeFunc={(e) => setPassword(e.nativeEvent.text)}
+          />
+          <CustomTextInput
+            placeHolderText="Şifre Tekrar"
+            value={rePassword}
+            onChangeFunc={(e) => setRePassword(e.nativeEvent.text)}
+          />
         </View>
         <View
           style={{
@@ -51,9 +79,70 @@ const Register = ({ navigation }) => {
           }}>
           <CustomButton
             buttonText={"Kayıt Ol"}
-            onPressFunc={() =>
-              navigation.navigate("CustomDrawerNavigator")
-            }></CustomButton>
+            onPressFunc={async () => {
+              const resp = await fetch(
+                "https://63660cb5046eddf1baf7a688.mockapi.io/api/v1/user"
+              );
+              const userCheckData = await resp.json();
+
+              let isUser = false;
+
+              for (let i = 0; i < userCheckData.length; i++) {
+                if (nick == userCheckData[i].nick) {
+                  isUser = true;
+                }
+              } // get yapıp kullanıcı var mı bak
+
+              if (isUser) {
+                ToastMessage("Kullanıcı Adı Zaten Kayıtlı");
+              } else {
+                // kullanıcı adı kayıtlı değilse post ile kayıt yap
+                // kontroller eksik
+
+                if (
+                  nick === "" ||
+                  userName === "" ||
+                  surname === "" ||
+                  phone === "" ||
+                  password === "" ||
+                  rePassword === ""
+                ) {
+                  ToastMessage("Lütfen Tüm Alanları Doldurunuz");
+                } else {
+                  const requestOptions = {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                      createdAt: new Date(),
+                      nick: nick,
+                      name: userName,
+                      lastName: surname,
+                      phone: phone,
+                      //company: "st merkezi",
+                      //unvan: "ünvan", // boş değer
+                      password: password,
+                    }),
+                  };
+                  try {
+                    const response = await fetch(
+                      "https://63660cb5046eddf1baf7a688.mockapi.io/api/v1/user",
+                      requestOptions
+                    );
+                    const data = await response.json();
+                    data
+                      .then(ToastMessage("Kayıt Başarılı"))
+                      .then(
+                        navigation.navigate("CustomDrawerNavigator", {
+                          user: data,
+                        })
+                      );
+                  } catch (error) {
+                    ToastMessage("Error registering user");
+                  }
+                }
+              }
+            }}
+          />
         </View>
         <TextButton
           onPressFunc={() => navigation.replace("Login")}
