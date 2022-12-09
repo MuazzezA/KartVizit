7956d21eb88c89217f9f4cfe87be2abb406999f7
MuazzazA <ayd.muazzez@gmail.com>
2022-12-09 14:36:34 +0300

    theme taşındı utils/constants

--- app/utils/constants/index.js
+++ app/utils/constants/index.js
@@ -1,5 +1,4 @@
 import images from "./images";
-import icons from "./icons";
 import theme, { COLORS, SIZES, FONTS } from "./theme";
 
-export { images, icons, theme, COLORS, SIZES, FONTS };
+export { images, theme, COLORS, SIZES, FONTS };
