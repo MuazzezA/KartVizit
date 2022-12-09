9867068644b5e27332b2dce10f84660b4482a930
MuazzazA <ayd.muazzez@gmail.com>
2022-12-09 14:44:08 +0300

    alert screen taşındı

--- app/screens/AlertScreen/AlertScreen.js
+++ app/screens/AlertScreen/AlertScreen.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Text, View } from "react-native";
+import styles from "./styles";
 
 const AlertScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={styles.container}>
       <Text>bildirim ekranı</Text>
     </View>
   );
 };
-export default AlertScreen;
+export { AlertScreen };
